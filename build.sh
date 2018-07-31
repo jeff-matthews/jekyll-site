b4c29ad62d034e086f40db9c29d052402c8aafe4
Jeff Matthews <matthews.jeffery@gmail.com>
2018-07-31 14:23:39 -0500

    use --branches option to match SHA

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ set -x
 set -e
 
 # This succeeds, but results in a detached head since CodeBuilde appears to be checking out the commit SHA. This returns "HEAD" as the branch name and uploads files to a HEAD directory in S3.
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# Get the first matching branch for the checked out SHA
+BRANCH=$(git rev-parse --abbrev-ref --branches HEAD | head -1)
 
 if [ "$BRANCH" != "master" ]; then
   echo 'Deploy to a staging directory';
