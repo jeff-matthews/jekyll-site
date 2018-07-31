3d14d0afd295bee00fa1adc10b95f32331619ba9
Jeff Matthews <matthews.jeffery@gmail.com>
2018-07-31 14:51:40 -0500

    revised James' suggestion

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ set -e
 # This succeeds, but results in a detached head since CodeBuilde appears to be checking out the commit SHA. This returns "HEAD" as the branch name and uploads files to a HEAD directory in S3.
 # BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # Not quite sure what this does yet, but James said to try it.
-BRANCH=$(git branch | head -1 | awk '{split($0,a,"origin/"); split(a[2],b,")"); print b[1]}')
+BRANCH=$(git branch | head -1 | awk '{split($0,a,"at "); split(a[2],b,")"); print b[1]}')
 
 if [ "$BRANCH" != "master" ]; then
   echo 'Deploy to a staging directory';
