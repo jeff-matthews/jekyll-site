f1ba267f88287d839d8dbb052b0a873df8be42f9
Jeff Matthews <matthews.jeffery@gmail.com>
2018-07-31 14:53:59 -0500

    new approach to ID commit first

--- build.sh
+++ build.sh
@@ -8,8 +8,9 @@ set -e
 
 # This succeeds, but results in a detached head since CodeBuilde appears to be checking out the commit SHA. This returns "HEAD" as the branch name and uploads files to a HEAD directory in S3.
 # BRANCH=$(git rev-parse --abbrev-ref HEAD)
-# Not quite sure what this does yet, but James said to try it.
-BRANCH=$(git branch | head -1 | awk '{split($0,a,"at "); split(a[2],b,")"); print b[1]}')
+# Identify the SHA, then ID the branch. Could be problematic if the SHA belongs to multiple branches...
+COMMIT=$(git rev-parse --verify HEAD)
+BRANCH=$(git branch --contains $COMMIT)
 
 if [ "$BRANCH" != "master" ]; then
   echo 'Deploy to a staging directory';
