0cc6ab36d4fee49c5acfac47485de642bfd4d661
Jeff Matthews <matthews.jeffery@gmail.com>
2018-07-31 12:26:00 -0500

    this command succeeds, but the branch var is always HEAD

--- build.sh
+++ build.sh
@@ -7,12 +7,7 @@
 set -e
 
 # This succeeds, but returns "HEAD" as the branch name and uploads files to a HEAD directory in S3.
-# BRANCH=$(git rev-parse --abbrev-ref HEAD)
-# This fails
-# BRANCH=$(git branch)
-# This fails with "fatal: ref HEAD is not a symbolic ref"
-# BRANCH=$(git symbolic-ref --short HEAD)
-BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$BRANCH" != "master" ]; then
   echo 'Deploy to a staging directory';
