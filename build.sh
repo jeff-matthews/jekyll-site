869fa2cf43b531673693b0da5198acd212c7f0d9
Jeff Matthews <matthews.jeffery@gmail.com>
2018-07-31 15:29:53 -0500

    revert back to production build/deploy only

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ set -e
 # This succeeds, but results in a detached head since CodeBuilde appears to be checking out the commit SHA. This returns "HEAD" as the branch name and uploads files to a HEAD directory in S3.
 # BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # Identify the SHA, then ID the branch. Could be problematic if the SHA belongs to multiple branches...
-COMMIT=$(git rev-parse --verify HEAD)
-BRANCH=$(git branch --contains $COMMIT | grep \* | cut -d ' ' -f2)
+# COMMIT=$(git rev-parse --verify HEAD)
+# BRANCH=$(git branch --contains $COMMIT | grep \* | cut -d ' ' -f2)
 
 if [ "$BRANCH" != "master" ]; then
   echo 'Deploy to a staging directory';
