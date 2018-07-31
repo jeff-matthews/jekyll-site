c4b01e1ee46369d96975a54c2551044d9e8f63fc
Jeff Matthews <matthews.jeffery@gmail.com>
2018-07-31 14:18:08 -0500

    stop using the buil script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -x
 # set "fail on error" in bash
 set -e
 
-# This succeeds, but returns "HEAD" as the branch name and uploads files to a HEAD directory in S3.
+# This succeeds, but results in a detached head since CodeBuilde appears to be checking out the commit SHA. This returns "HEAD" as the branch name and uploads files to a HEAD directory in S3.
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$BRANCH" != "master" ]; then
