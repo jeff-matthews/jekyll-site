a0b4154f42fd916e818909fa63581207b8230c4d
Jeff Matthews <matthews.jeffery@gmail.com>
2018-07-31 15:35:54 -0500

    testing build.sh on Travis CI

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -x
 set -e
 
 # This succeeds, but results in a detached head since CodeBuilde appears to be checking out the commit SHA. This returns "HEAD" as the branch name and uploads files to a HEAD directory in S3.
-# BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # Identify the SHA, then ID the branch. Could be problematic if the SHA belongs to multiple branches...
 # COMMIT=$(git rev-parse --verify HEAD)
 # BRANCH=$(git branch --contains $COMMIT | grep \* | cut -d ' ' -f2)
